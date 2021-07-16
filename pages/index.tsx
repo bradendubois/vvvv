eff05bd4a9955b1d24622d9936aa92c7a5f4b685
Braden Dubois <braden.dubois@usask.ca>
2021-07-15 18:17:21 -0600

    clean stylesheets, improve graphing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,28 @@
 import {useEffect, useState} from "react"
-import { LineChart, Line } from "recharts"
+import {LineChart, Line, YAxis, XAxis, CartesianGrid, Tooltip, Legend} from "recharts"
 
 // @ts-ignore
 // TODO
-import DatePicker from "react-datepicker";
+import ReactDatePicker from "react-datepicker";
 
 import {DailyReport, MapContext, useMap} from "../util/map_interface";
 import {OpenCOVID} from "../util/api_codes";
 
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss'
 import "react-datepicker/dist/react-datepicker.css";
 
+
+const DatePicker = ({...params}) => {
+
+    const context = useMap()
+
+    return (
+        <ReactDatePicker
+            {...params}
+            todayButton={true}
+        />
+    )
+}
 
 const Postcard = ({ province }: { province: OpenCOVID }) => {
 
@@ -28,10 +40,6 @@ const Home = () => {
 
     const context = useMap()
 
-    useEffect(() => {
-        console.log("ComeOn", context.COVIDData, context.COVIDData.length)
-    }, [context.COVIDData])
-
     return (
 
         <div className={styles.container}>
@@ -43,10 +51,28 @@ const Home = () => {
             <DatePicker minDate={context.lowerValid} maxDate={context.dateUpper ?? context.upperValid} selected={context.dateLower} onChange={(date: Date) => context.setDateLower(date)} />
             <DatePicker minDate={context.dateLower ?? context.lowerValid} maxDate={context.upperValid} selected={context.dateUpper} onChange={(date: Date) => context.setDateUpper(date)} />
 
-            <LineChart width={400} height={400} data={context.COVIDData}>
-                <Line type={"monotone"} dataKey={"cases"}/>
+            <LineChart width={1000} height={400} data={context.COVIDData}>
+                <Legend verticalAlign={"top"} />
+                <Tooltip />
+
+                <Line name={"Active Cases"} yAxisId={"L"} type={"monotone"} dataKey={"active_cases"} stroke={"#8884d8"}/>
+                <CartesianGrid scale={1} stroke={"#ccc"}/>
+                <XAxis dataKey={"date"}/>
+                <YAxis yAxisId={"L"} orientation={"left"}/>
+
+                <Line  yAxisId={"R"} type={"monotone"} dataKey={"cumulative_avaccine"}/>
+                <YAxis yAxisId={"R"} orientation={"right"}/>
             </LineChart>
 
+            {/*
+            <LineChart width={1000} height={400} data={context.COVIDData}>
+                <CartesianGrid stroke={"#ccc"}/>
+                <XAxis dataKey={"date"}/>
+                <YAxis />
+                <Tooltip />
+            </LineChart>
+*/}
+            {/* Picker of Canadian regions */}
             <main className={styles.main}>
                 <Postcard province={OpenCOVID.Yukon} />
                 <Postcard province={OpenCOVID.NorthwestTerritories} />
