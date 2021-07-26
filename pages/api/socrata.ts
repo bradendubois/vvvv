69dd45a2dbc0e326cb6c3eb8e1bfff6c0693920c
Braden Dubois <braden.dubois@usask.ca>
2021-07-26 11:19:22 -0600

    socrata data filtered to reduce network load

--- pages/api/socrata.ts
+++ pages/api/socrata.ts
@@ -4,7 +4,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
     // let url = `https://data.cdc.gov/resource/8xkx-amqh.json?$where=date%20between%20%272020-01-01T00:00:00%27%20and%20%27${today.getFullYear()}-${today.getMonth()+1}-${today.getDate()}T00:00:00%27`
 
-    let vaccination_url = `https://data.cdc.gov/resource/unsk-b7fc.json?$$app_token=${process.env.SOCRATA_TOKEN}&$limit=999999999&$order=date%20ASC`
+    let vaccination_url = `https://data.cdc.gov/resource/unsk-b7fc.json?$$app_token=${process.env.SOCRATA_TOKEN}&$limit=999999999&$order=date%20ASC&$select=date,location,administered_dose1_recip,series_complete_yes,administered_dose1_pop_pct,series_complete_pop_pct`
     let vaccination = await fetch(vaccination_url).then(response => response.json())
 
     res.status(200).json({
