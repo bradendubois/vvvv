a8ebcb854318606b09b8d57ad3fa7effa2ee568c
Braden Dubois <braden.dubois@usask.ca>
2021-08-04 17:57:29 -0600

    changed detection of production build

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 const { PHASE_PRODUCTION_BUILD } = require('next/constants')
+const currentGitBranchName = require("current-git-branch")
 
 module.exports = (phase, { defaultConfig }) => {
 
     return {
         env: {
-            production: phase === PHASE_PRODUCTION_BUILD
+            production: currentGitBranchName() === 'main'
         }
     }
 }
