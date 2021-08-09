5bb9241d654f9acdb526734bd32855cb91d12b7d
Braden Dubois <braden.dubois@usask.ca>
2021-08-09 14:55:13 -0600

    updated firebase secret layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,21 @@
 import type { AppProps } from 'next/app'
+import { useEffect } from "react";
+
 import { MapProvider } from "../util/context/provider";
 
 import firebase from 'firebase';
 import "firebase/analytics"
 
 import '../styles/globals.scss'
-import { useEffect } from "react";
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
 };
 
 const analytics = firebase.analytics
